38850012e5313a64810276dbd75d8d2cccc078fc
ivymmurithi <ivymurithi@gmail.com>
2022-04-20 23:25:07 +0300

    added Table header and body custom function components

--- src/Table.js
+++ src/Table.js
@@ -1,38 +1,57 @@
 import React, {Component} from "react";
 
-// We capitalize custom components to differentiate 
-// them from regular HTML elements
+/**
+ * ?We capitalize custom components to differentiate
+ * ?them from regular HTML elements
+ */
+
+/**
+ * ?A class component must include render(), and the 
+ * ?return can only return one parent element.
+ */
 class Table extends Component {
     render() {
         return (
             <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Job</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Charlie</td>
-                        <td>Janitor</td>
-                    </tr>
-                    <tr>
-                        <td>Mac</td>
-                        <td>Bouncer</td>
-                    </tr>
-                    <tr>
-                        <td>Dee</td>
-                        <td>Aspiring actress</td>
-                    </tr>
-                    <tr>
-                        <td>Dennis</td>
-                        <td>Bartender</td>
-                    </tr>
-                </tbody>
+                <TableHeader></TableHeader>
+                <TableBody></TableBody>
             </table>
         )
     }
+}
+
+const TableHeader = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Job</th>
+            </tr>
+        </thead>
+    )
+}
+
+const TableBody = () => {
+    return (
+    <tbody>
+        <tr>
+            <td>Charlie</td>
+            <td>Janitor</td>
+        </tr>
+        <tr>
+            <td>Mac</td>
+            <td>Bouncer</td>
+        </tr>
+        <tr>
+            <td>Dee</td>
+            <td>Aspiring actress</td>
+        </tr>
+        <tr>
+            <td>Dennis</td>
+            <td>Bartender</td>
+        </tr>
+    </tbody>
+    )
 }
 
 export default Table
