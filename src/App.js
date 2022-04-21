9e2846342729576e07f436a8fb6d22daf5d61ff6
ivymmurithi <ivymurithi@gmail.com>
2022-04-21 15:24:10 +0300

    state usage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React, {Component} from "react";
 import Table from "./Table";
 
-// Class Component
+// ? Class Component
+// ? class App extends Component {
+// ?   render() {
+// ?     const characters = [
+// ?       {
+// ?         name: 'Charlie',
+// ?         job: 'Janitor',
+// ?       },
+// ?       {
+// ?         name: 'Mac',
+// ?         job: 'Bouncer',
+// ?       },
+// ?       {
+// ?         name: 'Dee',
+// ?         job: 'Aspring actress',
+// ?       },
+// ?       {
+// ?         name: 'Dennis',
+// ?         job: 'Bartender',
+// ?       },
+// ?     ]
+
+// ?     return (
+// ?        <div className="container">
+//?              <Table characterData={characters}/>
+//?         </div> 
+// ?     )
+// ?   }
+// ? }
+
 class App extends Component {
-  render() {
-    const characters = [
-      {
+  // ?The state is the set of data that is managed by the component.
+  state = {
+    characters: [
+       {
         name: 'Charlie',
         job: 'Janitor',
       },
@@ -21,14 +51,29 @@ class App extends Component {
         name: 'Dennis',
         job: 'Bartender',
       },
-    ]
-
-    return (
-      <div className="container">
-        <Table characterData={characters}/>
-      </div>
-    )
+    ],
   }
+  
+  removeCharacter = (index) => {
+    const {characters} = this.state
+
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      }),
+    })
+    }
+
+    render() {
+      const {characters} = this.state
+
+      return (
+        <div>
+          <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        </div>
+      )
+    }
+
 }
 
 export default App
--- src/Table.js
+++ src/Table.js
@@ -9,18 +9,29 @@ import React, {Component} from "react";
  * ?A class component must include render(), and the 
  * ?return can only return one parent element.
  */
-class Table extends Component {
-    render() {
+// class Table extends Component {
+//     render() {
 
-        // ?we can access all props through this.props
-        const {characterData} = this.props
-        return (
-            <table>
-                <TableHeader/>
-                <TableBody characterData={characterData}/>
-            </table>
-        )
-    }
+//         // ?we can access all props through this.props
+//         const {characterData} = this.props
+//         return (
+//             <table>
+//                 <TableHeader/>
+//                 <TableBody characterData={characterData}/>
+//             </table>
+//         )
+//     }
+// }
+
+const Table = (props) => {
+    const {characterData, removeCharacter} = props
+
+    return (
+        <table>
+            <TableHeader/>
+            <TableBody characterData={characterData} removeCharacter={removeCharacter}/>
+        </table>
+    )
 }
 
 const TableHeader = () => {
@@ -40,6 +51,13 @@ const TableBody = (props) => {
             <tr key={index}>
                 <td>{row.name}</td>
                 <td>{row.job}</td>
+                <td>
+                    
+                    {/* //*The onClick function must pass through a function that returns 
+                    //*the removeCharacter() method, otherwise it will try to run automatically. */}
+
+                    <button onClick={() => props.removeCharacter(index)}>Delete</button>
+                </td>
             </tr>
         )
     })
